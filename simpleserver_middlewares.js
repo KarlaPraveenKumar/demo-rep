f18b2eab6281795d5204b6fa05b9ebd2c6e7c4d1
karlapraveenkumar <157881195+karlapraveenkumar@users.noreply.github.com>
2024-01-30 11:35:53 +0530

    Create simpleserver_middlewares.js

---
+++ simpleserver_middlewares.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+
+function usermiddleware(req,res,next){
+  const username = req.headers.username;
+  const password = req.headers.password;
+  if((username != "praveen") || (password != "pass")){
+    res.status(400).json({msg:"Something wrong with your inputs!"});
+  }
+  else{
+    next();
+  } 
+}
+
+function kidneymiddleware(req,res,next){
+  const kidneyid = req.query.kidneyid;
+  if(kidneyid != 1 && kidneyid != 2){
+    res.status(400).json({msg:"Something wrong with your inputs!"});
+  }
+  else{
+    next();
+  }
+}
+
+app.get("/health-check",usermiddleware,kidneymiddleware,function(req,res){
+  res.status(200).json({msg:"Healthy!"});
+});
+
+app.listen(3000);
